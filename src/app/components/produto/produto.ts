1bdc099ef2f4661d3fc3f4487968aac93779cc9a
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2026-05-07 16:51:12 -0300

    M05 ok

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [CurrencyPipe, UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
   nome = 'Notebook';
   preco = 3499.99;
+  mostrarPreco = true;
+
+  produtos = [
+    { nome: 'Monitor', preco: 1500 },
+    { nome: 'Mouse', preco: 150 },
+    { nome: 'Teclado', preco: 250 },
+  ];
 }
